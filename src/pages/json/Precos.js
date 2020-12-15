2f2454b6a3b056f179b38686a1708843fe9e4433
[johann] <johannteixeira2@gmail.com>
2020-12-15 00:01:14 -0300

    versao atualizada

---
+++ src/pages/json/Precos.js
@@ -0,0 +1,21 @@
+[
+    {
+        "1coluna": "Preço do Plano",
+        "2coluna":"R$ 9,99",
+        "3coluna":"2 dias",
+        "4coluna":"1(a sua escolha) e 2 jogos(sorteados)"
+    },
+    {
+        "1coluna":"Duração",
+        "2coluna":"R$ 14,99",
+        "3coluna":"15 dias",
+        "4coluna":"3(a sua escolha) e 2 jogos(sorteados)"
+
+    },
+    {
+        "1coluna":"Jogos disponíveis",
+        "2coluna":"R$ 24,99",
+        "3coluna":"30 dias",
+        "4coluna":"10(a sua escolha) e 10 jogos(sorteados)"
+    }
+]
